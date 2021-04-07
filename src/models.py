c059e35076810adc3591fe0d5f32cad59c09c41d
Adrian Acosta <aacrealms@hotmail.com>
2021-04-07 02:18:35 +0000

    Creates model

--- src/models.py
+++ src/models.py
@@ -8,23 +8,32 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = "user"
+    # Here we define columns for the table User
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    username = Column(String(40), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = "character"
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(10), nullable=False)
+    eye_color = Column(String(15), nullable=False)
+    hair_color = Column(String(15), nullable=False)
+
+
+class Planet(Base):
+    __tablename__ = "planet"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String(150), nullable=False)
+    climate = Column(String(150), nullable=False)
 
     def to_dict(self):
         return {}
